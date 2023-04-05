9ab3fc16f0c609f2255f5cabdf4ed71e821161d1
Gianella Leon <giigl@hotmail.com>
2023-04-04 19:33:15 -0500

    añadiendo una ruta

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,17 @@
-import app from "./app";
-app.listen(3000,()=>{
-    console.log('server on port 3000')
-})
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+import { port } from './config';
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Hello World'
+  });
+});
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`API started at http://localhost:${port}`);
+});
