c3ff79b8048a4fd2ab560dc7487e5d053a2be07e
Gianella Leon <giigl@hotmail.com>
2023-04-04 22:27:00 -0500

    añadiendo cookie-parser

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,15 +6,16 @@ import sequelize from './database';
 import allRoutes from './routes/index';
 import morgan from "morgan";
 import cors from "cors";
-
+import cookieParser from 'cookie-parser';
+//necesitas instalar los @types de todas las dependencias o si no saldra error 
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true , limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use('/', allRoutes);
 
 sequelize.sync({ force: true}).then(() => {
