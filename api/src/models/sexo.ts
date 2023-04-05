104616990319f33907ffd766f0d2a4b34078c0da
Gianella Leon <giigl@hotmail.com>
2023-04-04 22:49:17 -0500

    tabla sexo

---
+++ api/src/models/sexo.ts
@@ -0,0 +1,33 @@
+import { Model,DataTypes } from "sequelize";
+import sequelize from "../database";
+
+interface SexoAttributes{
+    id?:number;
+    name:string;
+}
+
+class Sexo extends Model <SexoAttributes> implements SexoAttributes{
+    public id?:number;
+    public name!: string;
+    public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Sexo.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        }
+    },{
+        sequelize,
+        tableName: "sexos",
+    }
+)
+
+export default Sexo;
