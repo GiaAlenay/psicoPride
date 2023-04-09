639e0c805cf5408e8a44315e069d1ec76aab4442
Gianella Leon <giigl@hotmail.com>
2023-04-08 23:28:33 -0500

    arreglar sexos get

--- api/src/controllers/sexo.ts
+++ api/src/controllers/sexo.ts
@@ -3,15 +3,18 @@ import { Request, Response } from 'express';
 import Sexo from '../models/sexo';
 import { sexArray } from '../data';
 export const findOrCreateSexs = async (req: Request, res: Response) => {
-    try {  
+    try {
+        
         sexArray.map(async(s)=>{
             await Sexo.findOrCreate({
                 where:{name:s.name }
             })
         })
-      res.status(201).json({ sexos: 'creados' });
+        const sexos= await Sexo.findAll()
+      
+      res.status(201).json(sexos);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Error creating Sexs' });
+      res.status(500).json({ message: 'Error creando Sexos' });
     }
   };
---
+++ client/src/reduxToolkit/reducers/sexo.ts
@@ -0,0 +1,54 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppThunk } from '../store';
+import axios  from 'axios';
+
+interface Sexo {
+  id: number;
+  name: string;
+}
+
+export interface SexoState {
+  data: Sexo[];
+  loading: boolean;
+  error: boolean;
+}
+
+const initialState: SexoState = {
+  data: [],
+  loading: false,
+  error: false,
+};
+
+const sexoSlice = createSlice({
+  name: 'sexo',
+  initialState,
+  reducers: {
+    getSexoStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    getSexoSuccess: (state, action: PayloadAction<any>) => {
+      state.data = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    getSexoError: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { getSexoStart, getSexoSuccess, getSexoError } = sexoSlice.actions;
+
+export const getSexos = (): AppThunk => async (dispatch) => {
+  dispatch(getSexoStart());
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    dispatch(getSexoSuccess(response.data));
+  } catch (err) {
+    dispatch(getSexoError());
+  }
+};
+
+export default sexoSlice.reducer;
