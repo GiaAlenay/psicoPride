c05918f75660c9be53a0bee4e3aaa4011393b463
Gianella Leon <giigl@hotmail.com>
2023-04-12 17:16:50 -0500

    refactorizando stepper

--- client/src/components/stepper/stepper.tsx
+++ client/src/components/stepper/stepper.tsx
@@ -2,32 +2,28 @@ import "./stepper.css";
 import { useState } from "react";
 
 export const Stepper = () => {
+  const stepsArray: number[] = [1, 2, 3, 4];
   const [steps, setSteps] = useState(1);
   return (
     <div className=" d-flex flex-row flex-wrap justify-content-center align-items-center mx-auto p-3">
       <div className="stepperCont d-flex flex-row flex-wrap justify-content-center align-items-center mx-auto p-3">
-        <button
-          className="numberStep"
-          onClick={() => {
-            setSteps(1);
-          }}
-        >
-          1
-        </button>
         <div className="progresLineStep">
           <div
-            className="progressDone"
-            style={{ width: `${steps === 1 ? "0%" : "100%"}` }}
+            className="progressDone "
+            style={{ width: `${(steps - 1) * 33.3}%` }}
           ></div>
         </div>
-        <button
-          className="numberStep"
-          onClick={() => {
-            setSteps(2);
-          }}
-        >
-          2
-        </button>
+        {stepsArray.map((s, i) => (
+          <button
+            key={i}
+            className={`numberStep numberStep${s}`}
+            onClick={() => {
+              setSteps(s);
+            }}
+          >
+            <h6>{s}</h6>
+          </button>
+        ))}
       </div>
     </div>
   );
