de2adbd0c12b646c99086229691aebbbab958294
Gianella Leon <giigl@hotmail.com>
2023-04-12 23:48:37 -0500

    landing avatar

--- client/src/components/chatPresentation/chatPresentations.tsx
+++ client/src/components/chatPresentation/chatPresentations.tsx
@@ -2,7 +2,13 @@ import "./chatPresentation.css";
 export const ChatPresentation = () => {
   return (
     <div className="chatPrePag">
-      <div className="centerCPP "></div>
+      <div className="centerCPP ">
+        <img src={"./logo.png"} alt={"logo"} className="logoPsicoPride" />
+        <div className="chatImgsCont">
+          <img src={"chats.png"} alt={"chats"} className="imgChats" />
+          <img src={"avatar.png"} alt={"avatar"} className="imgAvatar" />
+        </div>
+      </div>
     </div>
   );
 };
