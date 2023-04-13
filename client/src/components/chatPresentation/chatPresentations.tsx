9227b6ba6b97c0147806def58ef7db1a5a8ae9f7
Gianella Leon <giigl@hotmail.com>
2023-04-13 00:06:55 -0500

    working on landing

--- client/src/components/chatPresentation/chatPresentations.tsx
+++ client/src/components/chatPresentation/chatPresentations.tsx
@@ -8,6 +8,15 @@ export const ChatPresentation = () => {
           <img src={"chats.png"} alt={"chats"} className="imgChats" />
           <img src={"avatar.png"} alt={"avatar"} className="imgAvatar" />
         </div>
+        <div className="promptCont">
+          <div>
+            <h2 className="bienvenidos">¡Bienvenid@ a Dilo Sin Paltas!</h2>
+            <h3 className="moreinf">
+              Un lugar donde tus preguntas sobre sexualidad encontrarán las
+              respuestas correctas.
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
