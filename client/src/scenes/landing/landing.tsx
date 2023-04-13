c2ceb418fded82e8c35f97907d351b7f8419948b
Gianella Leon <giigl@hotmail.com>
2023-04-13 12:44:35 -0500

    about

--- client/src/scenes/landing/landing.tsx
+++ client/src/scenes/landing/landing.tsx
@@ -2,12 +2,14 @@ import { ChatPresentation } from "../../components/chatPresentation/chatPresenta
 import { useEffect, useRef, useState } from "react";
 import "./landing.css";
 import { ChatBurbuja } from "../../components/chatBurbuja/chatBurbuja";
+import { About } from "../../components/about/about";
 
 export const Landing = () => {
   return (
     <div className="landingPag">
       <ChatPresentation />
       <ChatBurbuja />
+      <About />
     </div>
   );
 };
