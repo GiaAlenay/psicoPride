0c892e610ec443a67b464933835604addb265774
Gianella Leon <giigl@hotmail.com>
2023-04-16 15:19:39 -0500

    scroll landing

--- client/src/scenes/landing/landing.tsx
+++ client/src/scenes/landing/landing.tsx
@@ -4,9 +4,22 @@ import "./landing.css";
 import { ChatBurbuja } from "../../components/chatBurbuja/chatBurbuja";
 import { About } from "../../components/about/about";
 import { Modal } from "../../components/loader/loaders";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
 export const Landing = () => {
   const [modal, setModal] = useState<boolean>(false);
+  const scrollbarsRef = useRef<Scrollbars>(null);
+
+  const thumbVerticalStyles = {
+    backgroundColor: "rgba(50, 50, 93, 0.6)",
+    borderRadius: 10,
+    width: "100px",
+  };
+
+  const renderThumbVertical = ({ style, ...props }: any) => {
+    const finalStyle = { ...style, ...thumbVerticalStyles };
+    return <div style={finalStyle} {...props} />;
+  };
   // useEffect(() => {
   //   localStorage.removeItem("SexoId");
   //   localStorage.removeItem("age");
@@ -15,14 +28,20 @@ export const Landing = () => {
   // }, []);
   return (
     <div className="landingPag">
-      <Modal open={modal} option={"loader"} />
-      <ChatPresentation />
-      <ChatBurbuja
-        setLoader={() => {
-          setModal(true);
-        }}
-      />
-      <About />
+      <Scrollbars
+        ref={scrollbarsRef}
+        style={{ width: "100%", height: "100%" }}
+        renderThumbVertical={renderThumbVertical}
+      >
+        <Modal open={modal} option={"loader"} />
+        <ChatPresentation />
+        <ChatBurbuja
+          setLoader={() => {
+            setModal(true);
+          }}
+        />
+        <About />
+      </Scrollbars>
     </div>
   );
 };
