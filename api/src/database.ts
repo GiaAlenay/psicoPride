2e86b00f08d36fe33fdc633c430d1b46cec65d36
Gianella Leon <giigl@hotmail.com>
2023-04-17 14:39:20 -0500

    sequelize production set

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,16 +1,44 @@
 import { db_host, db_port, db_name, db_user, db_password } from './config';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  db_name,
-  db_user,
-  db_password,
-  {
-    host: db_host,
-    port: db_port,
-    dialect: "postgres",
-  }
-);
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: db_name,
+        dialect: "postgres",
+        host: db_host,
+        port: 5432,
+        username: db_user,
+        password: db_password,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(`postgres://${db_user}:${db_password}@${db_host}/psicopride`, {
+        logging: false, // set to console.log to see the raw SQL queries
+        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      });
+
+// const sequelize = new Sequelize(
+//   db_name,
+//   db_user,
+//   db_password,
+//   {
+//     host: db_host,
+//     port: db_port,
+//     dialect: "postgres",
+//   }
+// );
 
 
 
--- api/src/index.ts
+++ api/src/index.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use('/', allRoutes);
 
 sequelize.sync({ force: false}).then(() => {
-    app.listen(3000, () => {
-        console.log('Server started on port 3000');
+    app.listen(port, () => {
+        console.log(`Server started on ${port} 3000`);
     });
   });
