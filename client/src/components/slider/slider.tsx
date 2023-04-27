643118e9d6e8bc0a6818da31d8d739c08c4c6c97
Gianella Leon <giigl@hotmail.com>
2023-04-27 09:17:04 -0500

    carrusel

--- client/src/components/slider/slider.tsx
+++ client/src/components/slider/slider.tsx
@@ -14,19 +14,16 @@ export const SliderBiblioteca: React.FC<MyProps> = ({
   return (
     <div className="contSlider">
       <div className="contSliceCenter">
-        <div className="contMainImg">
-          <div>
-            <img src={mainImagen} alt={titulo} className="mainImgSlider" />
-          </div>
-        </div>
-        <div className="contitleSlider">
+        <img src={mainImagen} alt={titulo} className="mainImgSlider" />
+
+        {/* <div className="contitleSlider">
           <div>{titulo}</div>
         </div>
         <div className="w-100 d-flex justify-content-center align-items-center">
           <button className="rounded-circle buttonMoreSlider">
             <AiOutlinePlus size={32} className="moreIconSlider" />
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
