64a46671d9431cd1f83f44c7cd5ad8220f253976
Gianella Leon <giigl@hotmail.com>
2023-04-27 20:28:45 -0500

    arreglado condiciones

--- client/src/components/condiciones/condicones.tsx
+++ client/src/components/condiciones/condicones.tsx
@@ -27,23 +27,20 @@ export const Condiciones: React.FC<MyComponentProps> = ({ show, setShow }) => {
             <div>
               <div className="bubble text-left">
                 <div className="promptsModalCondi">
-                  <h4>Hola soy X </h4>
-                  <h6>
-                    Te acompañaré mientras navegas tus dudas sobre sexualidad.
-                  </h6>
+                  <h4>HOLA, SOY CONI {`:)`} </h4>
                   <h6>
                     Quiero conocerte un poco mejor para ayudarte. ¡Y no te
                     preocupes! Nuestra conversación es completamente
                     confidencial.
                   </h6>
-                  <div className="w-100 d-flex flex-row flex-wrap justify-content-center align-items-center mx-auto p-3">
-                    <button onClick={setShow} className="closeModalBtn">
-                      Aceptar
-                    </button>
-                  </div>
                 </div>
               </div>
               <div className="pointer"></div>
+              <div className="notDisplayAcepBtn w-100 flex-row flex-wrap justify-content-center align-items-center mx-auto ">
+                <button onClick={setShow} className="closeModalBtn">
+                  Aceptar
+                </button>
+              </div>
             </div>
             <div className="avatarContModalHidden">
               <div>
@@ -52,6 +49,11 @@ export const Condiciones: React.FC<MyComponentProps> = ({ show, setShow }) => {
                   alt={"avatar"}
                   className="avatrModalHidden"
                 />
+              </div>
+              <div className="w-100 d-flex flex-row flex-wrap justify-content-center align-items-center mx-auto p-3">
+                <button onClick={setShow} className="closeModalBtn">
+                  Aceptar
+                </button>
               </div>
             </div>
           </div>
